d2ad8b939d52429c073c4462930970c77b5415b7
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-09-13 15:13:49 +0200

    Main zmieniony - można wyświetlać komunikat w konsoli

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,29 @@
 package com.example.demo;
 
+import com.example.demo.controller.ConstructorGreetingController;
+import com.example.demo.controller.PropertyGreetingController;
+import com.example.demo.controller.SetterGreetingController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+	ApplicationContext ctx = SpringApplication.run(DemoApplication.class, args);
 
+		PropertyGreetingController propertyGreetingController = (PropertyGreetingController) ctx.getBean("propertyGreetingController");
+		System.out.println("-------------Pole");
+		System.out.println(propertyGreetingController.getGreeting());
+
+		ConstructorGreetingController constructorGreetingController = (ConstructorGreetingController) ctx.getBean("constructorGreetingController");
+		System.out.println("-------------Constructor");
+		System.out.println(constructorGreetingController.getGreeting());
+
+		SetterGreetingController setterGreetingController = (SetterGreetingController) ctx.getBean("setterGreetingController");
+		System.out.println("---------------Setter");
+		System.out.println(setterGreetingController.getGreeting());
+
+	}
 }
