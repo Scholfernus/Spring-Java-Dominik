6428db04dd81b43ef63230c7dfc13031be75b503
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-09-13 15:12:59 +0200

    Constructor poprawki kodu

--- src/main/java/com/example/demo/controller/ConstructorGreetingController.java
+++ src/main/java/com/example/demo/controller/ConstructorGreetingController.java
@@ -1,13 +1,14 @@
 package com.example.demo.controller;
 
 import com.example.demo.repository.GreetingRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ConstructorGreetingController {
     private final GreetingRepository greeting;
 
-    public ConstructorGreetingController(GreetingRepository greeting) {
+    public ConstructorGreetingController(@Qualifier("constructorGreetingService") GreetingRepository greeting) {
         this.greeting = greeting;
     }
     public String getGreeting(){
--- src/main/java/com/example/demo/service/ConstructorGreetingService.java
+++ src/main/java/com/example/demo/service/ConstructorGreetingService.java
@@ -1,9 +1,11 @@
 package com.example.demo.service;
 
 import com.example.demo.repository.GreetingRepository;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 @Service
+//@Primary
 public class ConstructorGreetingService implements GreetingRepository {
     @Override
     public String sayGreeting() {
