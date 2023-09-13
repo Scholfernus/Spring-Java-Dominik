f500f3cd8533a74bcc19f7d30321bdc49d1bd34d
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-09-13 19:41:34 +0200

    Skomentowany @Service

--- src/main/java/com/example/demo/service/PropertyGreetingService.java
+++ src/main/java/com/example/demo/service/PropertyGreetingService.java
@@ -3,7 +3,7 @@ package com.example.demo.service;
 import com.example.demo.repository.GreetingRepository;
 import org.springframework.stereotype.Service;
 
-@Service
+//@Service
 public class PropertyGreetingService implements GreetingRepository {
     @Override
     public String sayGreeting() {
